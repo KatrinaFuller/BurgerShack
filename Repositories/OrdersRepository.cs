41ad740553b9858caa7ef229889feabf45ca67b8
Katrina Fuller <trinafuller@att.net>
2019-10-16 21:15:18 -0600

    filled out ordersRepository

--- Repositories/OrdersRepository.cs
+++ Repositories/OrdersRepository.cs
@@ -1,54 +1,90 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using BurgerShack.Models;
+using Dapper;
 
 namespace BurgerShack.Repositories
 {
   public class OrdersRepository
   {
-    internal IEnumerable<Order> Get()
+    private readonly IDbConnection _db;
+
+    public OrdersRepository(IDbConnection db)
     {
-      throw new NotImplementedException();
+      _db = db;
     }
 
-    internal Order Get(int orderId)
+    public IEnumerable<Order> Get()
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM orders";
+      return _db.Query<Order>(sql);
     }
 
-    internal IEnumerable<Item> GetItemsByOrderId(int orderId)
+    public Order Get(int id)
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM orders WHERE id = @id";
+      return _db.QueryFirstOrDefault<Order>(sql, new { id });
     }
 
-    internal int Create(Order newOrder)
+    public int Create(Order newOrder)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO orders
+      (customerName)
+      VALUES
+      (@CustomerName);
+      SELECT LAST_INSERT_ID();
+      ";
+      return _db.ExecuteScalar<int>(sql, newOrder); //getting the new id back and returning it
     }
 
-    internal void Edit(Order order)
+    public void Edit(Order order)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      UPDATE orders
+      SET
+        customerName = @CustomerName
+    WHERE id = @Id";
+      _db.Execute(sql, order);
+    }
+    public void Delete(int id)
+    {
+      string sql = "DELETE FROM orders WHERE id = $id";
+      _db.Execute(sql, new { id });
     }
 
-    internal void AddItemToOrder(int id, string itemId)
+    public void AddItemToOrder(int orderId, string itemId)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO itemsorders
+      (orderId, itemId)
+      VALUES
+      (@orderId, @itemId)";
+      _db.Execute(sql, new { orderId, itemId });
     }
 
-    internal ItemOrder GetItemOrder(ItemOrder itemOrder)
+    public IEnumerable<Item> GetItemsByOrderId(int orderId)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      SELECT * FROM itemsorders io
+      INNER JOIN items ON i.id = io.itemId
+      WHERE io.orderId = @orderId
+      ";
+      return _db.Query<Item>(sql, new { orderId });
     }
 
-    internal void RemoveItemFromOrder(int id)
+    public ItemOrder GetItemOrder(ItemOrder itemOrder)
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM itemsorders WHERE orderId = @OrderId AND itemId = @ItemId";
+      return _db.QueryFirstOrDefault<ItemOrder>(sql, itemOrder);
     }
 
-    internal void Delete(int id)
+    public void RemoveItemFromOrder(int id)
     {
-      throw new NotImplementedException();
+      string sql = "DELETE FROM itemsorders WHERE id = @id";
+      _db.Execute(sql, new { id });
     }
+
   }
 }
