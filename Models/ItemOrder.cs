ebf0ddb1c549da74b7f5c4ee1a906c80e2514eaf
Katrina Fuller <trinafuller@att.net>
2019-10-16 20:43:43 -0600

    itemOrder

---
+++ Models/ItemOrder.cs
@@ -0,0 +1,9 @@
+namespace BurgerShack.Models
+{
+  public class ItemOrder  //helper class
+  {
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public string ItemId { get; set; }
+  }
+}
