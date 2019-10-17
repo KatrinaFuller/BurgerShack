cd65c78cf10a8ed0c59bb174b745dbc767d91b9f
Katrina Fuller <trinafuller@att.net>
2019-10-16 18:52:23 -0600

    filled out ItemsController

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BurgerShack.Models;
+using BurgerShack.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +13,77 @@ namespace BurgerShack.Controllers
   [Route("api/[controller]")]
   public class ItemsController : ControllerBase
   {
+    private readonly ItemsService _itemsService;
+    public ItemsController(ItemsService itemsService)
+    {
+      _itemsService = itemsService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Item>> Get()
+    {
+      try
+      {
+        return Ok(_itemsService.Get());
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Item> Get(string id)
+    {
+      try
+      {
+        return Ok(_itemsService.Get(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Item> Create([FromBody] Item newItem)
+    {
+      try
+      {
+        return Ok(_itemsService.Create(newItem));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Item> Edit([FromBody] Item newItem, string id)
+    {
+      try
+      {
+        newItem.Id = id;
+        return Ok(_itemsService.Edit(newItem));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<string> Delete(string id)
+    {
+      try
+      {
+        return Ok(_itemsService.Delete(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
 
   }
 }
---
+++ Services/ItemsService.cs
@@ -0,0 +1,33 @@
+using System;
+using BurgerShack.Models;
+
+namespace BurgerShack.Services
+{
+  public class ItemsService
+  {
+    internal object Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Get(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Create(Item newItem)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Item newItem)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Delete(string id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
