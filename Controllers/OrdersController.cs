e5148ea6bf3552698006ad8e5ce0972cbd769bc2
Katrina Fuller <trinafuller@att.net>
2019-10-16 20:40:49 -0600

    filled out ordersController

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using BurgerShack.Models;
+using BurgerShack.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerShack.Controllers
+{
+  [ApiController]
+  [Route("/api/[controller]")]
+
+  public class OrdersController : ControllerBase
+  {
+    private readonly OrdersService _ordersService;
+    public OrdersController(OrdersService ordersService)
+    {
+      _ordersService = ordersService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Order>> Get()
+    {
+      try
+      {
+        return Ok(_ordersService.Get());
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Order> Get(int id)
+    {
+      try
+      {
+        return Ok(_ordersService.Get(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}/items")]
+    public ActionResult<Order> GetItems(int id)
+    {
+      try
+      {
+        return Ok(_ordersService.GetItems(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Order> Create([FromBody] Order newOrder)
+    {
+      try
+      {
+        return Ok(_ordersService.Create(newOrder));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Order> Edit([FromBody] Order newOrder, int id)
+    {
+      try
+      {
+        newOrder.Id = id;
+        return Ok(_ordersService.Edit(newOrder));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}/addItem")]
+    public ActionResult<string> AddItemToOrder([FromBody] Item item, int id)
+    {
+      try
+      {
+        return Ok(_ordersService.AddItemToOrder(id, item.Id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}/removeItem")]
+    public ActionResult<string> RemoveShoeFromOrder([FromBody] ShoeOrder shoeOrder, int id)
+    {
+      try
+      {
+        shoeOrder.OrderId = id;
+        return Ok(_ordersService.RemoveShoeFromOrder(shoeOrder));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<string> Delete(int id)
+    {
+      try
+      {
+        return Ok(_ordersService.Delete(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+
+
+  }
+}
---
+++ Services/OrdersService.cs
@@ -0,0 +1,48 @@
+using System;
+using BurgerShack.Models;
+
+namespace BurgerShack.Services
+{
+  public class OrdersService
+  {
+    internal object Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object GetItems(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Create(Order newOrder)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Order newOrder)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object AddItemToOrder(int id1, string id2)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object RemoveShoeFromOrder(ShoeOrder shoeOrder)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
